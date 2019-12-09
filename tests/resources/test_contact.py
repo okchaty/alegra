54302ed645b5806470fae641ab2052566baa2726
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2019-12-09 02:29:46 -0500

    feat: add test for contacts

---
+++ tests/resources/test_contact.py
@@ -0,0 +1,41 @@
+import alegra
+
+
+class TestContact:
+    def test_crud(self):
+        alegra.user = "developer@okchaty.com"
+        alegra.token = "45a1bcb2cc25d9c60196"
+        # List contacts.
+        response = alegra.Contact.list()
+        assert response.status_code == 200
+        # Create contact.
+        response = alegra.Contact.create(
+            name="Chaty",
+            identification={
+                "type": "RUC",
+                "number": "20000000000",
+            },
+            email="developer@okchaty.com",
+            type=["client"],
+            address={
+                "address": "Jr. Neptuno 777",
+                "city": "Lima, Peru",
+            },
+        )
+        assert response.status_code == 201
+        # Retrieve contact.
+        contact_id = response.json().get("id")
+        response = alegra.Contact.retrieve(contact_id)
+        assert response.status_code == 200
+        # Modify contact.
+        response = alegra.Contact.modify(
+            resource_id=contact_id,
+            identification={
+                "type": "RUC",
+                "number": "20000000001",
+            },
+        )
+        assert response.status_code == 200
+        # Delete contact.
+        response = alegra.Contact.delete(contact_id)
+        assert response.status_code == 200
