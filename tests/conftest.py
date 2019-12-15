a67f2ec92a22c4e3eb2149d3c0c894a2f3a30fe4
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2019-12-14 23:29:50 -0500

    fix: test credentials

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import pytest
 
 @pytest.fixture(autouse=True)
 def setup_alegra():
-    alegra.user = "developer@okchaty.com"
+    alegra.user = "develop@okchaty.com"
     alegra.token = "45a1bcb2cc25d9c60196"
